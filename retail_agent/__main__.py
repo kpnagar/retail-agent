0392ea601917536bb5a99151a84771aa3dab1826
siddhant-mi <siddhant.pandey@mindinventory.com>
2024-04-04 15:50:41 +0530

    Implemented TTS

--- retail_agent/__main__.py
+++ retail_agent/__main__.py
@@ -2,6 +2,9 @@ import speech_recognition
 import tools as t
 from routing_layer import rl
 from pynput import keyboard as kb
+from gtts import gTTS
+from io import BytesIO
+import pygame
 
 
 def on_release(key):
@@ -16,11 +19,27 @@ def listen_for_input(recognizer):
         return recognizer.recognize_whisper(audio_data=audio, model="small.en") or input("\nYou: ")
 
 
+def text_to_speech(text: str) -> None:
+    tts = gTTS(text=text, lang='en')
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+
 def process_input(message):
     print("Processing...")
     out = rl(message)
-    response = get_response(out)
+    if out.name is not None:
+        response = get_response(out)
+    else:
+        response = t.chitchat(message)
     print(f"Agent: {response}")
+    text_to_speech(response)
 
 
 def get_response(out):
@@ -31,15 +50,16 @@ def get_response(out):
         'price_tracking': t.price_tracking,
         'order_tracking': t.order_tracking
     }
-    response_function = response_functions.get(out.name, t.chitchat)
+    response_function = response_functions.get(out.name)
     return response_function(**out.function_call)
 
 
 def main():
     recording_started = False
     recognizer = speech_recognition.Recognizer()
-    print("Agent: Hello, how may I help you with your shopping today?")
-
+    initial_message = "Hello, how may I help you with your shopping today?"
+    print("Agent: ", initial_message)
+    text_to_speech(initial_message)
     recording_started = False
 
     def on_press(key):
